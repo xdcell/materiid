38239d2df4d9d7dc714e67ea677f8e0f62f445cc
xdcell <40189887+xdcell@users.noreply.github.com>
2019-07-28 23:03:58 +0700

    Update zero.js

--- zero.js
+++ zero.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://www.materiid.site/2019/07/safelink-aku.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.materiid.site/2019/07/safelink-aku.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
